f6dc04453e8153bb72ef42248e5c840ceea881bd
9Syahrul915 <sn081349462627@gmail.com>
2026-04-27 16:31:52 +0800

    eldeer

---
+++ ldr_random.py
@@ -0,0 +1,20 @@
+# Simulasi Sensor LDR - Python
+# Jalankan: python ldr_random.py
+# Hentikan: Ctrl+C
+
+import random
+import time
+
+while True:
+    nilai = random.randint(0, 4095)  # Nilai acak LDR (0 - 4095)
+
+    print(f"Nilai LDR: {nilai}", end=" -> ")
+
+    if nilai < 1000:
+        print("GELAP")
+    elif nilai < 3000:
+        print("REDUP")
+    else:
+        print("TERANG")
+
+    time.sleep(0.5)
