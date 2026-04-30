ea73d8f591ef3554d37950cd040324baf3c927d7
byyaja <2410819220006@mhs.ulm.ac.id>
2026-04-30 15:00:55 +0800

    uts

--- ldr_sensor.ino
+++ ldr_sensor.ino
@@ -6,8 +6,6 @@ void setup() {
   // Memulai komunikasi serial dengan baud rate 115200 (Standar ESP32)
   Serial.begin(115200);
   
-  // Mengatur pin digital LDR sebagai input
-  pinMode(PIN_DIGITAL, INPUT);
   
   Serial.println("Memulai pembacaan sensor LDR...");
   delay(1000);
@@ -17,18 +15,18 @@ void loop() {
   // 1. Membaca Nilai Analog (Output: 0 - 4095 pada ESP32)
   int nilaiCahaya = analogRead(PIN_ANALOG);
   
-  // 2. Membaca Nilai Digital (Output: 0 atau 1)
-  int statusGelap = digitalRead(PIN_DIGITAL);
 
   // Menampilkan hasil ke Serial Monitor
-  Serial.print("Nilai Analog: ");
+  Serial.print("Nilai cahaya: ");
   Serial.print(nilaiCahaya);
-  
+
   Serial.print(" | Status: ");
   // Umumnya DO bernilai 1 (HIGH) jika gelap, dan 0 (LOW) jika terang.
   // Ini bisa berbeda tergantung modul, silakan disesuaikan jika terbalik.
-  if (statusGelap == HIGH) {
+  if (nilaiCahaya < 500) {
     Serial.println("Gelap");
+  } else if (nilaiCahaya < 1500) {
+    Serial.println("Redup");
   } else {
     Serial.println("Terang");
   }
